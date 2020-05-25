2819d5c98debffff90a8c551902a61a9e6b1ce58
morghayn <morgan.nolan@hotmail.com>
2020-05-25 15:52:36 +0100

    Second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
+import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class Main
+public class Main extends Application
 {
     @Override
     public void start(Stage primaryStage)
